9236a13c0ac242a19a7b105970b2436360810889
TJKT Student <gzberhanif@gmail.com>
2026-08-06 14:40:02 +0700

    Update login system V8

--- auth.js
+++ auth.js
@@ -1,54 +1,31 @@
-const users = [
-
-{
-username:"Musyaffa Hanif Sunni",
-kelas:"XI TJKT 2",
-password:"musyaffa123",
-role:"admin"
-},
-
-{
-username:"Anjay Alok",
-kelas:"XI TJKT 4",
-password:"alok123",
-role:"user"
-},
-
-{
-username:"Rizky",
-kelas:"XI TJKT 1",
-password:"rizky321",
-role:"user"
-}
-
-];
-
 function login(){
 
 const username=document.getElementById("username").value.trim();
 const kelas=document.getElementById("kelas").value.trim();
-const password=document.getElementById("password").value;
+const password=document.getElementById("password").value.trim();
 
-const user=users.find(u=>
-
-u.username.toLowerCase()==username.toLowerCase()&&
-u.kelas.toLowerCase()==kelas.toLowerCase()&&
-u.password==password
-
-);
-
-if(!user){
-
-document.getElementById("loginStatus").innerHTML=
-"❌ Nama, kelas, atau password salah!";
-
+if(username==="" || kelas===""){
+document.getElementById("loginStatus").innerHTML="❌ Nama dan kelas wajib diisi!";
 return;
-
 }
 
-localStorage.setItem("tnos_username",user.username);
-localStorage.setItem("tnos_kelas",user.kelas);
-localStorage.setItem("tnos_role",user.role);
+if(password!=="tnos2026"){
+document.getElementById("loginStatus").innerHTML="❌ Password salah!";
+return;
+}
+
+let role="user";
+
+if(
+username.toLowerCase()=="musyaffa hanif sunni" &&
+kelas.toLowerCase()=="xi tjkt 2"
+){
+role="admin";
+}
+
+localStorage.setItem("tnos_username",username);
+localStorage.setItem("tnos_kelas",kelas);
+localStorage.setItem("tnos_role",role);
 
 window.location.href="index.html";
 
